e7dacd1fd05d6db75291c693f58cb1c6ca504333
mbrennerr <marciobrennerbusiness@gmail.com>
2022-03-11 17:14:45 -0300

    Add Tupla_examples

---
+++ Tupla.ts
@@ -0,0 +1,9 @@
+let fullName: [string, string] = ["M", "Brenner"];
+let person: [string, number] = ["MB", 35];
+let car: [string, string, number] = ["Ford","F400",10];
+
+console.log(fullName);
+console.log(person);
+console.log(car);
+
+/*Permite declarar um conjunto de  vaalores cuja ordem e tipo dos valores são fixas;*/
