74219595bf0afca672621ae139eef7871412c778
mbrennerr <marciobrennerbusiness@gmail.com>
2022-03-11 16:03:39 -0300

    Add arryas exemple

--- TypesFunctions.js
+++
@@ -1,9 +0,0 @@
-var lowd = function (value) {
-    if (typeof value === 'string')
-        return value.toUpperCase();
-    else if (typeof value === 'number')
-        return value.toString().toUpperCase();
-    return "";
-};
-console.log(lowd(1));
-console.log(lowd("teste"));
---
+++ arrays.ts
@@ -0,0 +1,8 @@
+let languages: string[] = [];
+let ids: number[] = [];
+
+languages.push('teste1'); //pass.
+languages.push(10); //falha.
+
+ids.push(100);//pass;
+ids.push('teste2');//falha;
